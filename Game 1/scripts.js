f799c7a9003870540d98091c5a1876508a132149
PeithonKing <amukherjeeniser@gmail.com>
2021-12-18 18:33:09 +0530

    refine

--- Game 1/scripts.js
+++ Game 1/scripts.js
@@ -1,18 +1,16 @@
 document.getElementById("startBTN").addEventListener("click", startGame);
 
-if (window.innerHeight > window.innerWidth) {
-    portrait = true;
-	console.log(portrait)
-} else {
-    portrait = false;
-	console.log(portrait)
-}
+function right_orientation(){
+	if (window.innerHeight > window.innerWidth) {
+		return false;
+	} else {return true;}
+};
 
 var instructions = [
-	"1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
-	"2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
-	"3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
-	"4. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores.",
 ]
 
 var questions = [
@@ -33,15 +31,22 @@ var questions = [
 	}
 ]
 
-
 function startGame() {
-	console.log("Clicked on start");
-    document.getElementById("startgame").style.visibility = "hidden";
-	setTimeout(showInstructions, 100)
+	if(right_orientation()){
+		// console.log("Clicked on start");
+		document.getElementById("startgame").style.visibility = "hidden";
+		setTimeout(showInstructions, 100)
+	}
+	else{
+		console.log("Change to Landscape")
+		document.getElementById("startgameH").style.animation = "shake 0.25s infinite";
+		setTimeout(() => {
+			document.getElementById("startgameH").style.animation = "";
+		}, 250)
+	}
 }
 	
 var atInst = 0; // defines the instruction levels completed
-	
 function showInstructions(){
 	document.getElementById("instrctions").style.visibility = "visible";
 	next();
@@ -49,20 +54,21 @@ function showInstructions(){
 	document.getElementById("skip").addEventListener("click", game);
 }
 
-
 function next(){
 	atInst += 1;
-	document.getElementById("text").innerHTML = instructions[atInst-1]
+	document.getElementById("text").innerHTML = `<p>Instructions:</p><p>${atInst}.&emsp;` + instructions[atInst-1] + "</p>"
 	if(atInst>instructions.length){game();}
 	console.log(atInst);
 }
 
-function game(){
+function game(){ask("Hi!");}
+
+function ask(q){
 	document.getElementById("helpText").style.visibility = "hidden";
 	document.getElementById("prof").style.visibility = "hidden";
-	document.getElementById("ship").style.transform = "translateY(65vh)";
-	document.getElementById("ship").style.visibility = "hidden";
+	document.getElementById("ship").style.animation = "descend 1s";
 	setTimeout(() => {
+		document.getElementById("ship").style.visibility = "hidden";
 		document.getElementById("game").style.visibility = "visible";
 		document.getElementById("game").innerHTML = '<div class="spaceship"></div><div class="asteroid"></div>';
     }, 1000)
